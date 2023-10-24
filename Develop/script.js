7c982f4d92373882f8ee34f38eeaa60000b02bb0
Patric Xiong <pathlub@gmail.com>
2023-10-23 21:58:38 -0400

    <Updating ReadMe>

--- Develop/script.js
+++ Develop/script.js
@@ -31,38 +31,31 @@ get.addEventListener("click", function () {
     document.getElementById("password").placeholder = ps;
 });
 
-// Start function to generate password
 function generatePassword() {
-    // Asks for user input
     enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
-    // First if statement for user validation 
     if (!enter) {
         alert("This needs a value");
     } else if (enter < 8 || enter > 128) {
-        // Validates user input
-        // Start user input prompts
         enter = parseInt(prompt("You must choose between 8 and 128"));
 
-    } else {
-        // Continues once user input is validated
+    } else 
         confirmNumber = confirm("Will this contain numbers?");
         confirmCharacter = confirm("Will this contain special characters?");
         confirmUppercase = confirm("Will this contain Uppercase letters?");
         confirmLowercase = confirm("Will this contain Lowercase letters?");
     };
 
-    // Else if for 4 negative options
+
     if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
         choices = alert("You must choose a criteria!");
 
     }
-    // First if statement that uses user input prompts to determine choices
-    // Else if for 4 positive options
+  
     else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
 
         choices = character.concat(number, alpha, alpha2);
     }
-    // Else if for 3 positive options
+
     else if (confirmCharacter && confirmNumber && confirmUppercase) {
         choices = character.concat(number, alpha2);
     }
@@ -75,7 +68,7 @@ function generatePassword() {
     else if (confirmNumber && confirmLowercase && confirmUppercase) {
         choices = number.concat(alpha, alpha2);
     }
-    // Else if for 2 positive options 
+
     else if (confirmCharacter && confirmNumber) {
         choices = character.concat(number);
 
@@ -94,7 +87,7 @@ function generatePassword() {
     } else if (confirmNumber && confirmUppercase) {
         choices = number.concat(alpha2);
     }
-    // Else if for 1 positive option
+
     else if (confirmCharacter) {
         choices = character;
     }
@@ -104,28 +97,25 @@ function generatePassword() {
     else if (confirmLowercase) {
         choices = alpha;
     }
-    // Created space variable to fill uppercase conversion
+  
     else if (confirmUppercase) {
         choices = space.concat(alpha2);
     };
 
-    // password variable is an array placeholder for user generated amount of length
+   
     var password = [];
 
-    // Start random selection variables:
-    // Random selection for all variables: 
+   
     for (var i = 0; i < enter; i++) {
         var pickChoices = choices[Math.floor(Math.random() * choices.length)];
         password.push(pickChoices);
     }
-    // This joins the password array and converts it to a string
-    // Worked with a tutor to incorporate this option
-    var ps = password.join("");
+ 
+    var ps = password.join(""); {
     UserInput(ps);
     return ps;
 }
-// This puts the password value into the textbox
-// Changed function input to use textcontent
+
 function UserInput(ps) {
     document.getElementById("password").textContent = ps;
 
@@ -135,9 +125,7 @@ var copy = document.querySelector("#copy");
 copy.addEventListener("click", function () {
     copyPassword();
 });
-// This copies the password value - works
-// Code example demonstrated in a youtube video: 
-// Source: https://youtu.be/9sT03jEwcaw
+
 function copyPassword() {
     document.getElementById("password").select();
     document.execCommand("Copy");
--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # HW03-Password-Generator
 Password Word Generator for Bootcamp
+
+Summary
+HTML and CSS and Javascript documents create a random password generator
+This project emphasizes the use of using Javascript to make dynamic changes to an HMTL document
+
+This project has the following features:
+A generate button
+After user data is collected, a random password will be generated using Javascript
+
+I prompted to choose from the following password criteria: 8 and 128 characters
+Password containing special characters, numbers, and uppercase
+Either 4 variables individual ones, or 3 with toLowerCase to Uppercase conversion
+The application should validate user input and ensure that at least one character type is selected.
+If, else if statement
+Once all prompts are answered, the user will be presented with a password matching the answered prompts. Displaying the generated password in an alert is acceptable, but attempt to write the password to the page instead.
+Event listener will determine the password output with function to populate the value into the test area.
+Copy execCommand in event listener
+This project has script features of:
+Variable declaration area
+An event listener (onclick) called generatePassword
+This will prompt the user for input between 8-128
+This variable is changed to an interger using ParseInt()
+This will validate that the input is a number within range, or is a number
+This then uses the input to determine the types (or choices) or letters of characters used, using an if statement
+This then assigns values to the variables using arrays for character, number or alphabet
+Another variable is created to concatenate the above variables
+A for loop will loop through the enter prompt until it reaches the number entered by user.
+A password variable takes the value from the for loop, and converts it to a string.
+The string value then populates into the text area for the user using a UserInput function.
+An event listener (onlick) has also been created for the copy to clipboard feauture.
+This project has media Queries for:
+max-width: 980px
+Adjusts body and container width
+max-width: 786px
+Adjusts body and container width
+Adjusts buttons
+max-width: 640px
+Adjusts body and container width
+Adjusts buttons to be centered and stacked
+
+
+Creator: 
+Patric Xiong
+NorthWestern BootCamp
